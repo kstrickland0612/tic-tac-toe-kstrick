0941b6709e21ee2eed269948598a54c9e83b5fcb
kstrickland0612 <kstrickland0612@gmail.com>
2019-04-04 15:54:24 -0400

    Add user-facing messaging for authentication
    added user-message class to html
    added jquery to handle messages based on user auth success/failure

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,44 +1,44 @@
 const store = require('../store.js')
 
 const signUpSuccess = function (data) {
-  console.log('sign up success ran with the data: ', data)
+  $('.user-message').text('Sign up success!')
   $('form').trigger('reset')
 }
 
 const signUpFail = function (data) {
-  console.log('sign up failure ran with the data: ', data)
+  $('.user-message').text('Sign up failed, please try again.')
   $('form').trigger('reset')
 }
 
 const signInSuccess = function (data) {
-  console.log('sign in success ran with the data: ', data)
+  $('.user-message').text('Sign in success!')
   $('form').trigger('reset')
   store.user = data.user
 }
 
 const signInFail = function (data) {
-  console.log('sign in failure ran with the data: ', data)
+  $('.user-message').text('Sign in failed, please try again.')
   $('form').trigger('reset')
 }
 
 const changePwSuccess = function (data) {
-  console.log('change pw success ran with the data: ', data)
+  $('.user-message').text('Change password success!')
   $('form').trigger('reset')
 }
 
 const changePwFail = function (data) {
-  console.log('change pw failure ran with the data: ', data)
+  $('.user-message').text('Change password failed, please try again.')
   $('form').trigger('reset')
 }
 
 const signOutSuccess = function (data) {
-  console.log('sign out success')
+  $('.user-message').text('Sign out success!')
   $('form').trigger('reset')
   store.user = null
 }
 
 const signOutFail = function (data) {
-  console.log('sign out failed')
+  $('.user-message').text('Sign out failed, please try again.')
   $('form').trigger('reset')
 }
 
