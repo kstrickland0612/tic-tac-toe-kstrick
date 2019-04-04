3360135d104e076850c510b055ca6d13d23b5b97
kstrickland0612 <kstrickland0612@gmail.com>
2019-04-03 20:12:52 -0400

    Add winner variable to hold the winner
    tested with console log and it works!

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,30 +4,30 @@
 const newGame = function () {
 
 }
-
+let winner = ''
 const currentBoard = ['', '', '', '', '', '', '', '', '']
 let currentPlayer = 'x'
 
 const checkForWin = function (currentBoard) {
   if ((currentBoard[0] === 'x' && currentBoard[1] === 'x' && currentBoard[2] === 'x') || (currentBoard[0] === 'o' && currentBoard[1] === 'o' && currentBoard[2] === 'o')) {
-    console.log('win')
+    winner = currentBoard[0]
   } else if ((currentBoard[3] === 'x' && currentBoard[5] === 'x' && currentBoard[5] === 'x') || (currentBoard[3] === 'o' && currentBoard[4] === 'o' && currentBoard[5] === 'o')) {
-    console.log('win')
+    winner = currentBoard[3]
   } else if ((currentBoard[6] === 'x' && currentBoard[7] === 'x' && currentBoard[8] === 'x') || (currentBoard[6] === 'o' && currentBoard[7] === 'o' && currentBoard[8] === 'o')) {
-    console.log('win')
+    winner = currentBoard[6]
   } else if ((currentBoard[0] === 'x' && currentBoard[4] === 'x' && currentBoard[8] === 'x') || (currentBoard[0] === 'o' && currentBoard[4] === 'o' && currentBoard[8] === 'o')) {
-    console.log('win')
+    winner = currentBoard[0]
   } else if ((currentBoard[0] === 'x' && currentBoard[3] === 'x' && currentBoard[6] === 'x') || (currentBoard[0] === 'o' && currentBoard[3] === 'o' && currentBoard[6] === 'o')) {
-    console.log('win')
+    winner = currentBoard[0]
   } else if ((currentBoard[1] === 'x' && currentBoard[4] === 'x' && currentBoard[7] === 'x') || (currentBoard[1] === 'o' && currentBoard[4] === 'o' && currentBoard[7] === 'o')) {
-    console.log('win')
+    winner = currentBoard[1]
   } else if ((currentBoard[2] === 'x' && currentBoard[5] === 'x' && currentBoard[8] === 'x') || (currentBoard[2] === 'o' && currentBoard[5] === 'o' && currentBoard[8] === 'o')) {
-    console.log('win')
+    winner = currentBoard[2]
   } else if ((currentBoard[2] === 'x' && currentBoard[4] === 'x' && currentBoard[6] === 'x') || (currentBoard[2] === 'o' && currentBoard[4] === 'o' && currentBoard[6] === 'o')) {
-    console.log('win')
+    winner = currentBoard[2]
   }
+  console.log(winner)
 }
-console.log(checkForWin(currentBoard))
 
 const switchPlayer = function () {
   if (currentPlayer === 'x') {
